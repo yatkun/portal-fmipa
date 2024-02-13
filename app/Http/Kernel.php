aa3d1d2dd7038a2d5cd1ef9182198d02b7c43215
“yatkun” <sweetninenty99@gmail.com>
2024-02-13 16:49:48 +0800

    rev

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,3 +69,5 @@ class Kernel extends HttpKernel
         'checkRole' => \App\Http\Middleware\CheckRole::class,
     ];
 }
+
+
--- resources/views/elearning/dosen/detail.blade.php
+++ resources/views/elearning/dosen/detail.blade.php
@@ -102,24 +102,12 @@
                                 </div>
                                 <div class="tab-pane" id="profile1" role="tabpanel">
                                     <p class="mb-0">
-                                        Food truck fixie locavore, accusamus mcsweeney's marfa nulla
-                                        single-origin coffee squid. Exercitation +1 labore velit, blog
-                                        sartorial PBR leggings next level wes anderson artisan four loko
-                                        farm-to-table craft beer twee. Qui photo booth letterpress,
-                                        commodo enim craft beer mlkshk aliquip jean shorts ullamco ad
-                                        vinyl cillum PBR. Homo nostrud organic, assumenda labore
-                                        aesthetic magna delectus.
+                                        
                                     </p>
                                 </div>
                                 <div class="tab-pane" id="messages1" role="tabpanel">
                                     <p class="mb-0">
-                                        Etsy mixtape wayfarers, ethical wes anderson tofu before they
-                                        sold out mcsweeney's organic lomo retro fanny pack lo-fi
-                                        farm-to-table readymade. Messenger bag gentrify pitchfork
-                                        tattooed craft beer, iphone skateboard locavore carles etsy
-                                        salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
-                                        Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh
-                                        mi whatever gluten-free carles.
+                                        
                                     </p>
                                 </div>
 
