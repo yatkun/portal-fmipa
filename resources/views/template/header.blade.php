01bf8f412dd148da406e52f7ba1246245e0df489
“yatkun” <sweetninenty99@gmail.com>
2024-03-18 00:52:41 +0800

    rev : perbaiki logo portal

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -10,16 +10,16 @@
                     </span>
                     <span class="logo-lg">
                         
-                        <img src="{{ asset ('assets/images/logo-dark.png') }}" alt="" height="17">
+                        <img src="{{ asset ('assets/images/logo-dark.png') }}" alt="" height="35">
                     </span>
                 </a>
 
-                <a href="index.html" class="logo logo-light">
+                <a href="/" class="logo logo-light">
                     <span class="logo-sm">
                         <img src="{{ asset ('assets/images/logo.png') }}" alt="" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset ('assets/images/logo-light.png') }}" alt="" height="19">
+                        <img src="{{ asset ('assets/images/logo-light.png') }}" alt="" height="35">
                     </span>
                 </a>
             </div>
