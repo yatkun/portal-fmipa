c4a68e93b42c30ea5d9f1909635b7344f47609f7
“yatkun” <sweetninenty99@gmail.com>
2024-05-16 15:04:48 +0800

    ss

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -36,7 +36,7 @@
                     <i class="bx bx-fullscreen"></i>
                 </button>
             </div>
-            <div class="dropdown d-none d-lg-inline-block ms-1">
+            <div class="dropdown d-lg-inline-block ms-1">
                 <button type="button" class="btn header-item noti-icon waves-effect" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                     <i class="bx bx-customize"></i>
                 </button>
@@ -72,35 +72,7 @@
             </div>
 
             <div class="dropdown d-inline-block">
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end" style="position: absolute; inset: 0px 0px auto auto; margin: 0px; transform: translate(0px, 72px);" data-popper-placement="bottom-end">
-                    <div class="px-lg-2">
-                        <div class="row g-0">
-                            <div class="col">
-                                <a class="dropdown-icon-item" href="https://fmipa.unsulbar.ac.id" target="_blank">
-                                    <img src="assets/images/brands/github.png" alt="Github">
-                                    <span>Web FMIPA</span>
-                                </a>
-                            </div>
-                            <div class="col">
-                                <a class="dropdown-icon-item" href="https://repository.fmipa.unsulbar.ac.id" target="_blank">
-                                    <img src="assets/images/brands/bitbucket.png" alt="bitbucket">
-                                    <span>Repository FMIPA</span>
-                                </a>
-                            </div>
-                            
-                        </div>
-
-                        {{-- <div class="row g-0">
-                            <div class="col">
-                                <a class="dropdown-icon-item" href="#">
-                                    <img src="assets/images/brands/dropbox.png" alt="dropbox">
-                                    <span>Dropbox</span>
-                                </a>
-                            </div>
-                           
-                        </div> --}}
-                    </div>
-                </div>
+                
           
                 <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-notifications-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="bx bx-bell bx-tada"></i>
