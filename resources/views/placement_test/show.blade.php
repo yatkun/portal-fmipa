49c944367f23b223c88158dedee4a3c77f71aa85
“yatkun” <sweetninenty99@gmail.com>
2024-09-06 22:19:32 +0800

    update placement test

--- resources/views/placement_test/show.blade.php
+++ resources/views/placement_test/show.blade.php
@@ -12,29 +12,168 @@
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
 
-    <link rel="stylesheet" href="{{ asset ('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset ('assets/css/icons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset ('assets/css/app.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/icons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/tailwind.css') }}">
+    <style>
+        .account-pages {
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        #loader-wrapper {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 1000;
+            display: none;
+        }
+
+        #loader {
+            display: block;
+            position: relative;
+            left: 50%;
+            top: 50%;
+            width: 150px;
+            height: 150px;
+            margin: -75px 0 0 -75px;
+            border-radius: 50%;
+            border: 3px solid transparent;
+            border-top-color: #3498db;
+            -webkit-animation: spin 2s linear infinite;
+            /* Chrome, Opera 15+, Safari 5+ */
+            animation: spin 2s linear infinite;
+            /* Chrome, Firefox 16+, IE 10+, Opera */
+
+        }
+
+        #loader:before {
+            content: "";
+            position: absolute;
+            top: 5px;
+            left: 5px;
+            right: 5px;
+            bottom: 5px;
+            border-radius: 50%;
+            border: 3px solid transparent;
+            border-top-color: #e74c3c;
+            -webkit-animation: spin 3s linear infinite;
+            /* Chrome, Opera 15+, Safari 5+ */
+            animation: spin 3s linear infinite;
+            /* Chrome, Firefox 16+, IE 10+, Opera */
+
+        }
+
+        #loader:after {
+            content: "";
+            position: absolute;
+            top: 15px;
+            left: 15px;
+            right: 15px;
+            bottom: 15px;
+            border-radius: 50%;
+            border: 3px solid transparent;
+            border-top-color: #f9c922;
+            -webkit-animation: spin 1.5s linear infinite;
+            /* Chrome, Opera 15+, Safari 5+ */
+            animation: spin 1.5s linear infinite;
+            /* Chrome, Firefox 16+, IE 10+, Opera */
+
+        }
+
+        @-webkit-keyframes spin {
+            0% {
+                -webkit-transform: rotate(0deg);
+                /* Chrome, Opera 15+, Safari 3.1+ */
+                -ms-transform: rotate(0deg);
+                /* IE 9 */
+                transform: rotate(0deg);
+                /* Firefox 16+, IE 10+, Opera */
+            }
+
+            100% {
+                -webkit-transform: rotate(360deg);
+                /* Chrome, Opera 15+, Safari 3.1+ */
+                -ms-transform: rotate(360deg);
+                /* IE 9 */
+                transform: rotate(360deg);
+                /* Firefox 16+, IE 10+, Opera */
+            }
+        }
+
+        @keyframes spin {
+            0% {
+                -webkit-transform: rotate(0deg);
+                /* Chrome, Opera 15+, Safari 3.1+ */
+                -ms-transform: rotate(0deg);
+                /* IE 9 */
+                transform: rotate(0deg);
+                /* Firefox 16+, IE 10+, Opera */
+            }
+
+            100% {
+                -webkit-transform: rotate(360deg);
+                /* Chrome, Opera 15+, Safari 3.1+ */
+                -ms-transform: rotate(360deg);
+                /* IE 9 */
+                transform: rotate(360deg);
+                /* Firefox 16+, IE 10+, Opera */
+            }
+        }
+
+        .bg-grid {
+            opacity: 0.2;
+            background-image: linear-gradient(90deg, transparent 24%, rgba(0, 0, 0, 0.1) 25%, rgba(0, 0, 0, 0.1) 26%, transparent 27%, transparent 74%, rgba(0, 0, 0, 0.1) 75%, rgba(0, 0, 0, 0.1) 76%, transparent 77%),
+                linear-gradient(0deg, transparent 24%, rgba(0, 0, 0, 0.1) 25%, rgba(0, 0, 0, 0.1) 26%, transparent 27%, transparent 74%, rgba(0, 0, 0, 0.1) 75%, rgba(0, 0, 0, 0.1) 76%, transparent 77%);
+            background-size: 60px 60px;
+        }
+
+        .backdrop-blur-lg {
+            --tw-backdrop-blur: blur(16px);
+            -webkit-backdrop-filter: var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia);
+            backdrop-filter: var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia);
+        }
+        .glass-effect {
+  background: rgba(255, 255, 255, 0.2); /* Transparansi warna putih */
+  backdrop-filter: blur(10px); /* Efek blur di belakang elemen */
+  border-radius: 10px; /* Sudut melengkung */
+  border: 1px solid rgba(255, 255, 255, 0.3); /* Batasan semi-transparan */
+
+  padding: 10px; /* Padding untuk konten */
+}
+.text-lg {
+    font-size:2rem;
+    font-weight: 700;
+}
+    </style>
 
 </head>
 
 <body>
-    <div class="account-pages my-md-2 my-5 pt-sm-5">
-        <div class="container">
-           
-            <div class="row justify-content-center">
-                <div class="col-md-12 col-lg-8 col-xl-7">
-              
-                    <div class="card overflow-hidden">
+
+
+    <!-- end account-pages -->
+    <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+        <img src="https://play.tailwindcss.com/img/beams.jpg" alt=""
+            class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
+        <div class="absolute inset-0 bg-grid "></div>
+        <div class="relative mx-auto col-md-8 col-lg-6 col-xl-5 ">
+                <div class="glass-effect">
+                    <div class="card overflow-hidden" style="margin-bottom: 0px">
                         <div class="bg-primary bg-soft">
                             <div class="row">
                                 <div class="col-12">
                                     <div class="text-primary p-4">
-                                        <h1 class="text-primary font-bold">PENGUMUMAN HASIL PLACEMENT TEST 2024</h1>
+                                        <h1 class="text-lg text-primary font-bold" style="margin-bottom: 5px">PENGUMUMAN HASIL PLACEMENT TEST 2024</h1>
                                         <h5 class="text-secondary">PROGRAM STUDI STATISTIKA</h5>
                                     </div>
                                 </div>
-                             
+        
                             </div>
                         </div>
                         <div class="card-body pt-0">
@@ -79,34 +218,45 @@
                             </div>
 
                         </div>
-                    </div>
-                    <div class="mt-5 text-center">
 
-                        <div>
-                            
-                            <p>© <script>
-                                    document.write(new Date().getFullYear())
-                                </script> Portal FMIPA Unsulbar. Crafted with <i class="mdi mdi-heart text-danger"></i>
-                                by Yatkun.Studio</p>
-                        </div>
                     </div>
+                </div>
+                <div class="mt-5 text-center">
 
+                    <div>
+    
+                        <p>©
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script> Portal FMIPA Unsulbar. Crafted with <i
+                                class="mdi mdi-heart text-danger"></i>
+                            by Yatkun.Studio
+                        </p>
+                    </div>
                 </div>
             </div>
+            
+
+           
+
         </div>
     </div>
-    <!-- end account-pages -->
 
-    <script src="{{ asset ('assets/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset ('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset ('assets/libs/metismenu/metisMenu.min.js') }}"></script>
-    <script src="{{ asset ('assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset ('assets/libs/node-waves/waves.min.js') }}"></script>
-    <script src="{{ asset ('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-    <script src="{{ asset ('assets/js/pages/dashboard.init.js') }}"></script>
-    <script src="{{ asset ('assets/js/app.js') }}"></script>
-    <script src="{{ asset ('assets/js/pages/validation.init.js') }}"></script>
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+    </div>
 
 </body>
+<script>
+    // Add event listener to the form submission
+    document.getElementById("cekdata").addEventListener("submit", function() {
+        // Show the loader
+        document.getElementById("loader").style.display = "block";
+        document.getElementById("loader-wrapper").style.display = "block";
+
+        // Disable the submit button to prevent multiple submissions
+        document.getElementById("submitBtn").disabled = true;
+    });
+</script>
 
 </html>
